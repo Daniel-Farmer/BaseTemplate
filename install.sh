2f375bddd3eed65f7da4194cde7727c129880d42
Daniel Edward Farmer <67203566+Daniel-Farmer@users.noreply.github.com>
2025-04-25 02:00:00 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+# Update the system and install requirements
 sudo apt update -y -q
 sudo apt install jq -y -q
 
+# Create valid details.json
 cat <<EOF > /root/details.json
 {
     "userid": "danielfarmer",
@@ -11,8 +13,8 @@ cat <<EOF > /root/details.json
 }
 EOF
 
+# Extract projectid and create a directory
 project_dir=$(jq -r '.projectid' /root/details.json)
-
 mkdir -p "$project_dir"
 
 echo "Project directory created: $project_dir"
