012cbcbdcfb29566305ac2a5d95365e35dc1cf07
Daniel Edward Farmer <67203566+Daniel-Farmer@users.noreply.github.com>
2025-04-25 02:05:14 +0100

    Update and rename test1.sh to install.sh

--- install.sh
+++ install.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
-# Update the system and install requirements
+# Updates the system and installs dependencies.
 sudo apt update -y -q
 sudo apt install jq -y -q
 
-# Create valid details.json
+# Creates an example JSON file.
 cat <<EOF > /root/details.json
 {
     "userid": "danielfarmer",
     "projectid": "89422",
-    "prompt": "when the server starts say hello world"
+    "prompt": "when the server starts, say hello world"
 }
 EOF
 
-# Extract projectid and create a directory
+# Creates the project directory.
 project_dir=$(jq -r '.projectid' /root/details.json)
 mkdir -p "$project_dir"
 
-echo "Project directory created: $project_dir"
+# Removes the installation file.
+rm -r install.sh
